7f48bf4a393a9e8562c85c5aaf31a7a959bbb720
Isaiah Henson <Devpuddles@gmail.com>
2022-10-08 14:30:25 -0400

    made more progress

--- controllers/home.js
+++ controllers/home.js
@@ -1,33 +1,6 @@
 module.exports = {
-  getIndex: (req, res) => {
-    let i = 0 // Start point
-    let images = [] 
-    let time = 3000
-   
-    //Image List
-   images[0] = '../public/imgs/si_1.jpg'
-   images[1] = '../public/imgs/si_2.jpg'
-   images[2] = '../public/imgs/si_3.jpg'
-   images[3] = '../public/imgs/gd_1.jpg'
-   images[4] = '../public/imgs/gd_2.jpg'
-   images[5] = '../public/imgs/gd_3.jpg'
-   images[6] = '../public/imgs/gs_1.jpg'
-   images[7] = '../public/imgs/gs_2.jpg'
-   images[8] = '../public/imgs/gs_3.jpg'
+  getIndex: (req, res) => { 
 
-   // Change Image
-   function changeImg(){
-     document.slide.src = images[i];
-
-     if( i < images.length -1){
-       i++;
-     } else {
-       i = 0
-     }
-     
-     setTimeout("changeImg()", time)
-   }
-    
-    res.render("index.ejs");
+    res.render("index.ejs") 
   },
 };
